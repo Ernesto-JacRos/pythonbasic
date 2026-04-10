63e6efe578297ce5f33b52e1751166c1a9e5a455
Ernesto Jacros <jacrosernesto@gmail.com>
2026-04-09 20:12:31 -0600

    ejercicos de while e inicio de for

---
+++ 03_loops/02_loop_for.py
@@ -0,0 +1,11 @@
+###
+# 02 - Bucles (for)
+# Permite ejecutar un bloque de codigo repetidamente mientras ITERA un iterable o una lista
+###
+
+print('\nBucle for')
+#Ietrar una lista 
+frutas = ['manzana', 'pera', 'mandarina']
+
+for fruta in frutas:
+    print(fruta)
